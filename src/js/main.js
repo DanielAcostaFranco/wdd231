327bd6202a6b5da4566f1054762cb63bcc7f5178
DanielAcostaFranco <danielacosta@byui.edu>
2025-09-27 00:01:40 -0600

    Prove NPS 2

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,24 @@
-import { getParkData } from "./parkService.mjs";
+
+import { getParkData, parkInfoLinks } from "./parkService.mjs";
+import setHeaderFooter from "./setHeaderFooter.mjs";
+import { mediaCardTemplate } from "./templates.mjs";
 
 const parkData = getParkData();
 
-const disclaimer = document.querySelector(".disclaimer > a");
-disclaimer.href = parkData.url;
-disclaimer.innerHTML = parkData.fullName;
+function setParkIntro(data) {
+    const introEl = document.querySelector(".intro");
+    introEl.innerHTML = `<h1>${data.fullName}</h1>
+    <p>${data.description}</p>`;
+}
+
+function setParkInfoLinks(data) {
+    const infoEl = document.querySelector(".info");
+
+    const html = data.map(mediaCardTemplate);
+
+    infoEl.insertAdjacentHTML("afterbegin", html.join(""));
+}
+
+setHeaderFooter(parkData);
+setParkIntro(parkData);
+setParkInfoLinks(parkInfoLinks);
---
+++ src/js/setHeaderFooter.mjs
@@ -0,0 +1,30 @@
+import { footerTemplate } from "./templates.mjs";
+
+function setHeaderInfo(data) {
+    const disclaimer = document.querySelector(".disclaimer > a");
+    disclaimer.href = data.url;
+    disclaimer.textContent = data.fullName;
+
+    document.querySelector("head > title").textContent = data.fullName;
+
+    const bannerImg = document.querySelector("#hero-banner > img");
+    bannerImg.src = data.images[0].url;
+    bannerImg.alt = data.images[0].altText || data.fullName;
+
+    const heroContent = document.querySelector("#hero-text");
+    heroContent.innerHTML = `
+      <h1>${data.name}</h1>
+      <span>${data.designation}</span>
+      <span>${data.states}</span>
+  `;
+}
+
+function setFooter(data) {
+    const footerEl = document.querySelector("#park-footer");
+    footerEl.innerHTML = footerTemplate(data);
+}
+
+export default function setHeaderFooter(data) {
+    setHeaderInfo(data);
+    setFooter(data);
+}
