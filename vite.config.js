764582dc9907769671c122694fd798736b1f9dfe
DanielAcostaFranco <danielacosta@byui.edu>
2025-12-06 03:56:37 -0700

    dssdf

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "src/index.html"),
         conditions: resolve(__dirname, "src/conditions.html")
+        visitor_centers: resolve(__dirname, "src/visitor_centers.html"),
       }
     }
   }
